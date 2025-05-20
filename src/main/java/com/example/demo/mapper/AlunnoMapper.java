273ba1bf56c051e18ed3584178d8dc1d0203366d
Antonella Trulli <a.trulli@elitesoftwarehouse.com>
2025-05-20 15:20:25 +0200

    added mapstruct

--- src/main/java/com/example/demo/mapper/AlunnoMapper.java
+++ src/main/java/com/example/demo/mapper/AlunnoMapper.java
@@ -1,83 +1,30 @@
 package com.example.demo.mapper;
 
 import com.example.demo.dto.AlunnoDTO;
+import com.example.demo.dto.AlunnoWithoutCorsiDTO;
 import com.example.demo.entity.Alunno;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-@Component
-public class AlunnoMapper {
+@Mapper(componentModel = "spring")
+public interface AlunnoMapper {
 
-    @Autowired
-    CorsoMapper corsoMapper;
+    Alunno toEntity(AlunnoDTO alunnoDTO);
 
-    public AlunnoDTO convertEntityToDTO(Alunno alunno) {
-        AlunnoDTO alunnoDTO = new AlunnoDTO();
-        alunnoDTO.setId(alunno.getId());
-        alunnoDTO.setNome(alunno.getNome());
-        alunnoDTO.setCognome(alunno.getCognome());
-        alunnoDTO.setData(alunno.getData());
-        alunnoDTO.setCitta(alunno.getCitta());
-        alunnoDTO.setVoto(alunno.getVoto());
-        if (alunno.getCorsi() != null) {
-            alunnoDTO.setCorsi(corsoMapper.convertEntitiesNoAlunni(alunno.getCorsi()));
-        } else {
-            alunnoDTO.setCorsi(null);
-        }
-        return alunnoDTO;
-    }
+    AlunnoWithoutCorsiDTO toDtoWithoutCorsi(Alunno alunno);
 
-    public Alunno convertDTOToEntity(AlunnoDTO alunnoDTO) {
-        Alunno alunno = new Alunno();
-        alunno.setId(alunnoDTO.getId());
-        alunno.setNome(alunnoDTO.getNome());
-        alunno.setCognome(alunnoDTO.getCognome());
-        alunno.setData(alunnoDTO.getData());
-        alunno.setCitta(alunnoDTO.getCitta());
-        alunno.setVoto(alunnoDTO.getVoto());
-        if (alunnoDTO.getCorsi() != null) {
-            alunno.setCorsi(corsoMapper.convertDTONoAlunni(alunnoDTO.getCorsi()));
-        } else {
-            alunno.setCorsi(null);
-        }
-        return alunno;
-    }
+    AlunnoDTO toDto(Alunno alunno);
 
-    public Set<AlunnoDTO> convertAlunniEntityNoCorsi(Set<Alunno> alunni) {
-        Set<AlunnoDTO> alunniDTO = new HashSet<>();
-        AlunnoDTO alunnoDTO;
-        for(Alunno alunno:alunni){
-            alunnoDTO = new AlunnoDTO();
-            alunnoDTO.setId(alunno.getId());
-            alunnoDTO.setNome(alunno.getNome());
-            alunnoDTO.setCognome(alunno.getCognome());
-            alunnoDTO.setData(alunno.getData());
-            alunnoDTO.setCitta(alunno.getCitta());
-            alunnoDTO.setVoto(alunno.getVoto());
-            alunniDTO.add(alunnoDTO);
-        }
-        return alunniDTO;
-    }
+    List<Alunno> toEntity(List<AlunnoDTO> alunnoDTOs);
 
-    public Set<Alunno> convertAlunniDTONoCorsi(Set<AlunnoDTO> alunniDTO) {
-        Set<Alunno> alunni = new HashSet<>();
-        Alunno alunno;
-        for(AlunnoDTO alunnoDTO:alunniDTO){
-            alunno = new Alunno();
-            alunno.setId(alunnoDTO.getId());
-            alunno.setNome(alunnoDTO.getNome());
-            alunno.setCognome(alunnoDTO.getCognome());
-            alunno.setData(alunnoDTO.getData());
-            alunno.setCitta(alunnoDTO.getCitta());
-            alunno.setVoto(alunnoDTO.getVoto());
-            alunni.add(alunno);
-        }
+    List<AlunnoDTO> toDto(List<Alunno> alunni);
 
-        return alunni;
-    }
+    Set<AlunnoWithoutCorsiDTO> toDtoWithoutCorsi(Set<Alunno> alunni);
 
+    Alunno toEntityWithoutCorsi(AlunnoWithoutCorsiDTO alunnoWithoutCorsiDTO);
+
+    Set<Alunno> toEntityWithoutCorsi(Set<AlunnoWithoutCorsiDTO> alunnoWithoutCorsiDTOs);
 }
